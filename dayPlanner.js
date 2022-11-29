bf68291fbdd46761f31954fe728dbe62247a86ad
Ben <benackdrums@gmail.com>
2022-11-28 23:48:38 -0600

    initial commit

---
+++ dayPlanner.js
@@ -0,0 +1,35 @@
+var now = moment().format("dddd, MMMM do");
+
+document.getElementById("currentDay").innerHTML = now;
+
+
+
+// $('#9').addEventListener("click", console.log('hello world'));
+
+var element = document.getElementById("btn0");
+
+element.addEventListener("click", function() {
+    document.getElementById("row0").value = "Hello World";
+  });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var a = moment('2016-01-01'); 
+// var b = a.add(1, 'week'); 
+// a.format();
+
+// var c = a.format();
+// console.log(c);
